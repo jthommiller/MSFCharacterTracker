154f8e9b195678c07c17a5ef8b28b4d15eed1ab2
James Miller <james_miller314@mymail.eku.edu>
2020-07-20 02:25:06 -0400

    IOS Application initial creation

---
+++ MSFCharacterTrackerIOSApplication/MSFCharacterTrackerIOSApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MSFCharacterTrackerIOSApplication
+//
+//  Created by James Miller on 7/20/20.
+//  Copyright © 2020 James Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
